a5d835eb2d8a092d9984f2e1886dc6e4ed78bcfa
Alexander Nehmer <a.nehmer@science-computing.de>
2017-11-05 20:36:04 +0100

    REQU-101: moved code to NestedTemplateComponentResource and use TemplateRoot to access TemplateComponent
    Signed-off-by: Alexander Nehmer <a.nehmer@science-computing.de>

--- org.eclipse.mdm.businessobjects/src/main/java/org/eclipse/mdm/businessobjects/boundary/NestedTemplateComponentResource.java
+++ org.eclipse.mdm.businessobjects/src/main/java/org/eclipse/mdm/businessobjects/boundary/NestedTemplateComponentResource.java
@@ -15,6 +15,7 @@ import static org.eclipse.mdm.businessobjects.boundary.ResourceConstants.ENTITYA
 import static org.eclipse.mdm.businessobjects.boundary.ResourceConstants.REQUESTPARAM_CONTEXTTYPE;
 import static org.eclipse.mdm.businessobjects.boundary.ResourceConstants.REQUESTPARAM_ID;
 import static org.eclipse.mdm.businessobjects.boundary.ResourceConstants.REQUESTPARAM_ID2;
+import static org.eclipse.mdm.businessobjects.boundary.ResourceConstants.REQUESTPARAM_ID3;
 import static org.eclipse.mdm.businessobjects.boundary.ResourceConstants.REQUESTPARAM_SOURCENAME;
 
 import java.util.Map;
@@ -53,14 +54,15 @@ import io.vavr.control.Option;
 import io.vavr.control.Try;
 
 /**
- * {@link TemplateComponent} resource handling REST requests
+ * {@link TemplateComponent} resource handling REST requests for nested
+ * {@link TemplateComponent}s
  * 
  * @author Alexander Nehmer, science+computing AG Tuebingen (Atos SE)
  *
  */
 @Path("/environments/{" + REQUESTPARAM_SOURCENAME + "}/tplroots/{" + REQUESTPARAM_CONTEXTTYPE + "}/{" + REQUESTPARAM_ID
-		+ "}/tplcomps")
-public class TemplateComponentResource {
+		+ "}/tplcomps/{" + REQUESTPARAM_ID2 + "}/tplcomps")
+public class NestedTemplateComponentResource {
 
 	@EJB
 	private EntityService entityService;
@@ -79,19 +81,26 @@ public class TemplateComponentResource {
 	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{" + REQUESTPARAM_ID2 + "}")
+	@Path("/{" + REQUESTPARAM_ID3 + "}")
 	public Response find(@PathParam(REQUESTPARAM_SOURCENAME) String sourceName,
 			@PathParam(REQUESTPARAM_CONTEXTTYPE) String contextTypeParam, @PathParam(REQUESTPARAM_ID) String tplRootId,
-			@PathParam(REQUESTPARAM_ID2) String id) {
+			@PathParam(REQUESTPARAM_ID2) String parentTplCompId, @PathParam(REQUESTPARAM_ID3) String id) {
 		return Try.of(() -> ResourceHelper.mapContextType(contextTypeParam))
 				.map(contextType -> this.entityService.find(TemplateRoot.class, contextType, sourceName, tplRootId))
-				.map(ro -> ro.map(r -> Stream.ofAll(r.getTemplateComponents())
-						.find(c -> c.getID()
-								.equals(id))))
-				// TODO not so beautiful to do that get()
-				.map(to -> to.get())
+				// get matching parent
+				// TODO not so beautiful to do that get() at the end
+				.map(root -> root.map(r -> Stream.ofAll(r.getTemplateComponents())
+						.find(tplComp -> tplComp.getID()
+								.equals(parentTplCompId))
+						.get()))
+				// get nested tplComp
+				.map(parentTplComp -> parentTplComp.map(comp -> Stream.ofAll(comp.getTemplateComponents())
+						.find(nestedTplComp -> nestedTplComp.getID()
+								.equals(id))
+						.get())
+						.get())
 				// error messages from down the callstack? Use Exceptions or some Vavr magic?
-				.map(e -> new MDMEntityResponse(TemplateComponent.class, e.get()))
+				.map(e -> new MDMEntityResponse(TemplateComponent.class, e))
 				.map(r -> ServiceUtils.toResponse(r, Status.OK))
 				.onFailure(ResourceHelper.rethrowException)
 				// TODO send reponse or error regarding error expressiveness
@@ -115,19 +124,22 @@ public class TemplateComponentResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findAll(@PathParam(REQUESTPARAM_SOURCENAME) String sourceName,
 			@PathParam(REQUESTPARAM_CONTEXTTYPE) String contextTypeParam, @PathParam(REQUESTPARAM_ID) String tplRootId,
+			@PathParam(REQUESTPARAM_ID2) String parentTplCompId,
 			@QueryParam("filter") String filter) {
 		return Try.of(() -> ResourceHelper.mapContextType(contextTypeParam))
 				// find the TemplateRoot
 				.map(ct -> this.entityService.find(TemplateRoot.class, ct, sourceName, tplRootId))
+				// get matching parent
+				// TODO not so beautiful to do that get() at the end
+				.map(root -> root.map(r -> Stream.ofAll(r.getTemplateComponents())
+						.find(tplComp -> tplComp.getID()
+								.equals(parentTplCompId))
+						.get())
+						.get())
 				// find the TemplateComponents
-				// TODO findChildren finds also the recursive TplComps as implemented in
-				// EntityRequest.load(Filter):273
-				// How to deal with that? reduce RESTAPI just to root objects, so no TplComps,
-				// Attrs etc.
-				.map(parent -> this.entityService.findChildren(TemplateRoot.class, TemplateComponent.class,
-						ResourceHelper.mapContextType(contextTypeParam), sourceName, tplRootId))
+				.map(parentTplComp -> parentTplComp.getTemplateComponents())
 				// prepare the result
-				.map(e -> new MDMEntityResponse(TemplateComponent.class, e.toJavaList()))
+				.map(e -> new MDMEntityResponse(TemplateComponent.class, e))
 				.map(r -> ServiceUtils.toResponse(r, Status.OK))
 				.onFailure(ResourceHelper.rethrowException)
 				.get();
@@ -139,16 +151,17 @@ public class TemplateComponentResource {
 	 * 
 	 * @param body
 	 *            The {@link TemplateComponent} to create.
+	 * @param id
+	 *            the identifier of the parent {@link TemplateComponent}.
 	 * @return The created {@link TemplateComponent} as {@link Response}.
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	// TODO test with already existing TplComp -> error handling onFailure in
-	// EntityService seems not to trigger
 	public Response create(@PathParam(REQUESTPARAM_SOURCENAME) String sourceName,
 			@PathParam(REQUESTPARAM_CONTEXTTYPE) String contextTypeParam, @PathParam(REQUESTPARAM_ID) String tplRootId,
-			String body) {
+			@PathParam(REQUESTPARAM_ID2) String tplParentCompId, String body) {
+
 		// deserialize JSON into object map
 		@SuppressWarnings("unchecked")
 		Map<String, Object> mapping = (Map<String, Object>) Try
@@ -156,10 +169,6 @@ public class TemplateComponentResource {
 				}))
 				.get();
 
-		// TODO clean up that mess
-
-		// TODO error handling
-		// get name
 		Option<String> name = Try.of(() -> mapping.get(ENTITYATTRIBUTE_NAME)
 				.toString())
 				.toOption();
@@ -182,20 +191,33 @@ public class TemplateComponentResource {
 				.get();
 
 		// get template root
-		// TODO handle non-existing TplRoot
 		Option<TemplateRoot> tplRoot = Try
 				.of(() -> this.entityService.find(TemplateRoot.class, contextType.get(), sourceName, tplRootId))
 				.get();
 
+		// get parent component
+		// TODO use vavr
+		Option<TemplateComponent> tplParent = tplRoot.map(r -> r.getTemplateComponents()
+				.stream()
+				.filter(t -> t.getID()
+						.equals(tplParentCompId))
+				.findFirst()
+				.get());
+
+		// set template root in parent Comp
+		// TODO should not use that method from EntityService as that's background logic
+		tplParent.forEach(p -> EntityService.setParentEntity(p, tplRoot.get(), null));
+
 		// create
 		return Try
-				.of(() -> entityService
-						.create(TemplateComponent.class, sourceName, name.get(), tplRoot.get(), catComp.get())
+				.of(() -> this.entityService
+						.create(TemplateComponent.class, sourceName, name.get(), tplParent.get(), catComp.get())
 						.get())
 				.onFailure(ResourceHelper.rethrowException)
 				.map(entity -> ServiceUtils.toResponse(new MDMEntityResponse(TemplateComponent.class, entity),
 						Status.OK))
 				.get();
+
 	}
 
 	/**
@@ -213,9 +235,9 @@ public class TemplateComponentResource {
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/{" + REQUESTPARAM_ID2 + "}")
+	@Path("/{" + REQUESTPARAM_ID3 + "}")
 	public Response update(@PathParam(REQUESTPARAM_SOURCENAME) String sourceName,
-			@PathParam(REQUESTPARAM_CONTEXTTYPE) String contextTypeParam, @PathParam(REQUESTPARAM_ID2) String id,
+			@PathParam(REQUESTPARAM_CONTEXTTYPE) String contextTypeParam, @PathParam(REQUESTPARAM_ID3) String id,
 			String body) {
 		return ResourceHelper.deserializeJSON(body)
 				.map(valueMap -> this.entityService.update(TemplateComponent.class,
@@ -238,9 +260,9 @@ public class TemplateComponentResource {
 	 */
 	@DELETE
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{" + REQUESTPARAM_ID2 + "}")
+	@Path("/{" + REQUESTPARAM_ID3 + "}")
 	public Response delete(@PathParam(REQUESTPARAM_SOURCENAME) String sourceName,
-			@PathParam(REQUESTPARAM_CONTEXTTYPE) String contextTypeParam, @PathParam(REQUESTPARAM_ID2) String id) {
+			@PathParam(REQUESTPARAM_CONTEXTTYPE) String contextTypeParam, @PathParam(REQUESTPARAM_ID3) String id) {
 		return Try.of(() -> ResourceHelper.mapContextType(contextTypeParam))
 				.map(contextType -> this.entityService.delete(TemplateComponent.class, sourceName, contextType, id)
 						.get())
