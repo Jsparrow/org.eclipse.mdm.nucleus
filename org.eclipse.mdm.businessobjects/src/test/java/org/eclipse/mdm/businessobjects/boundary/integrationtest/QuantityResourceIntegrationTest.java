d58d51f06f4beaeca8a254c217be40adcce9697a
Alexander Nehmer <a.nehmer@science-computing.de>
2017-11-17 16:18:20 +0100

    REQU-101: working on PhysDimensionResource and QuantityResource (WIP)
    Signed-off-by: Alexander Nehmer <a.nehmer@science-computing.de>

---
+++ org.eclipse.mdm.businessobjects/src/test/java/org/eclipse/mdm/businessobjects/boundary/integrationtest/QuantityResourceIntegrationTest.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2017 science + computing AG Tuebingen (ATOS SE)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Alexander Nehmer - initial implementation
+ *******************************************************************************/
+package org.eclipse.mdm.businessobjects.boundary.integrationtest;
+
+import org.junit.BeforeClass;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * Test class for QuantityResource.
+ * 
+ * @author Alexander Nehmer, science+computing AG Tuebingen (Atos SE)
+ * @see EntityResourceIntegrationTest
+ *
+ */
+public class QuantityResourceIntegrationTest extends EntityResourceIntegrationTest {
+
+	@BeforeClass
+	public static void prepareTestData() {
+		getLogger().debug("Preparing QuantityResourceIntegrationTest");
+
+		// set up test data
+		setContextClass(QuantityResourceIntegrationTest.class);
+
+		putTestDataValue(TESTDATA_RESOURCE_URI, "/quantities");
+		putTestDataValue(TESTDATA_ENTITY_NAME, "testQuantity");
+		putTestDataValue(TESTDATA_ENTITY_TYPE, "Quantity");
+
+		JsonObject json = new JsonObject();
+		json.add("name", new JsonPrimitive(getTestDataValue(TESTDATA_ENTITY_NAME)));
+		// TODO anehmer on 2017-11-17: create unit (and physDim) instead of taking fixed
+		// one
+		json.add("unit", new JsonPrimitive("1"));
+		putTestDataValue(TESTDATA_CREATE_JSON_BODY, json.toString());
+	}
+}
