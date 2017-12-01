9ca3ca70495c3788dfe4ce0e3cf3c2abc99dcb9b
Alexander Nehmer <a.nehmer@science-computing.de>
2017-12-01 14:52:58 +0100

    REQU-101: removed unused fields from  ResourceConstants
    Signed-off-by: Alexander Nehmer <a.nehmer@science-computing.de>

--- org.eclipse.mdm.businessobjects/src/main/java/org/eclipse/mdm/businessobjects/boundary/ResourceConstants.java
+++ org.eclipse.mdm.businessobjects/src/main/java/org/eclipse/mdm/businessobjects/boundary/ResourceConstants.java
@@ -3,14 +3,12 @@ package org.eclipse.mdm.businessobjects.boundary;
 import org.eclipse.mdm.api.base.model.ContextType;
 import org.eclipse.mdm.api.base.model.Quantity;
 import org.eclipse.mdm.api.base.model.Unit;
-import org.eclipse.mdm.api.dflt.model.CatalogAttribute;
 import org.eclipse.mdm.api.dflt.model.CatalogComponent;
 import org.eclipse.mdm.api.dflt.model.CatalogSensor;
 import org.eclipse.mdm.api.dflt.model.TemplateAttribute;
 import org.eclipse.mdm.api.dflt.model.TemplateComponent;
 import org.eclipse.mdm.api.dflt.model.TemplateRoot;
 import org.eclipse.mdm.api.dflt.model.TemplateSensor;
-import org.eclipse.mdm.api.dflt.model.TemplateTest;
 import org.eclipse.mdm.api.dflt.model.TemplateTestStep;
 import org.eclipse.mdm.api.dflt.model.TemplateTestStepUsage;
 
@@ -85,12 +83,6 @@ public final class ResourceConstants {
 	public static final String ENTITYATTRIBUTE_CATALOGSENSOR_ID = "catalogsensor";
 
 	/**
-	 * Parameter holding the id of the {@link CatalogAttribute} of e.g. the
-	 * {@link TemplateAttribute} in the request body
-	 */
-	public static final String ENTITYATTRIBUTE_CATALOGATTRIBUTE_ID = "catalogattribute";
-
-	/**
 	 * Parameter holding the id of the {@link Quantity} of e.g. the {@link Unit} in
 	 * the request body
 	 */
@@ -109,43 +101,10 @@ public final class ResourceConstants {
 	public static final String ENTITYATTRIBUTE_PHYSICALDIMENSION_ID = "physicaldimension";
 
 	/**
-	 * Parameter holding the id of the {@link TemplateTest} of e.g. the
-	 * {@link TemplateTestStepUsage} in the request body
-	 */
-	public static final String ENTITYATTRIBUTE_TEMPLATETEST_ID = "templatetest";
-
-	/**
 	 * Parameter holding the id of the {@link TemplateTestStep} of e.g. the
 	 * {@link TemplateTestStepUsage} in the request body
 	 */
 	public static final String ENTITYATTRIBUTE_TEMPLATETESTSTEP_ID = "templateteststep";
-
-	/**
-	 * Parameter holding the id of the {@link TemplateRoot} of
-	 * {@link ContextType.UNITUNDERTEST} of e.g. the {@link TemplateTestStep} in the
-	 * request body
-	 */
-	public static final String ENTITYATTRIBUTE_TPLROOTUNITUNDERTEST_ID = "templaterootunitundertest";
-
-	/**
-	 * Parameter holding the id of the {@link TemplateRoot} of
-	 * {@link ContextType.TESTSEQUENCE} of e.g. the {@link TemplateTestStep} in the
-	 * request body
-	 */
-	public static final String ENTITYATTRIBUTE_TPLROOTTESTSEQUENCE_ID = "templateroottestsequence";
-
-	/**
-	 * Parameter holding the id of the {@link TemplateRoot} of
-	 * {@link ContextType.TESTEQUIPMMENT} of e.g. the {@link TemplateTestStep} in
-	 * the request body
-	 */
-	public static final String ENTITYATTRIBUTE_TPLROOTTESTEQUIPMENT_ID = "templateroottestequipment";
-
-	/**
-	 * Parameter holding the id of the {@link ValueList} of e.g. the
-	 * {@link CatalogAttribute} in the request body
-	 */
-	public static final String ENTITYATTRIBUTE_VALUELISTID = "valuelist";
 
 	/**
 	 * Just hide the default constructor
