6dfd46a69f20bfbbe4b9c61d37fd897ce05f853c
Matthias Koller <m.koller@peak-solution.de>
2018-08-09 16:37:02 +0200

    Added missing license header to ResourceConstants
    Signed-off-by: Matthias Koller <m.koller@peak-solution.de>

--- org.eclipse.mdm.businessobjects/src/main/java/org/eclipse/mdm/businessobjects/boundary/ResourceConstants.java
+++ org.eclipse.mdm.businessobjects/src/main/java/org/eclipse/mdm/businessobjects/boundary/ResourceConstants.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2018 science + computing AG Tuebingen (ATOS SE)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
 package org.eclipse.mdm.businessobjects.boundary;
 
 import org.eclipse.mdm.api.base.model.ContextType;
